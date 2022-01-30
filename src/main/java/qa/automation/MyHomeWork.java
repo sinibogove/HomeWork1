a55128b2cd14e436c7bd24424b96a42e8ba0b737
sinibogove <vdrangelov@gmail.com>
2022-01-30 15:13:43 +0200

    Initial commit

---
+++ src/main/java/qa/automation/MyHomeWork.java
@@ -0,0 +1,10 @@
+package qa.automation;
+
+import java.util.Scanner;
+
+public class MyHomeWork {
+    public static void main(String[] args){
+        Scanner scanner  = new Scanner(System.in);
+    }
+}
+
