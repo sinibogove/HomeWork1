e9167fc8bb6c6d6e14107e2232d6846256bd402f
sinibogove <vdrangelov@gmail.com>
2022-01-30 16:35:48 +0200

    Final Homework1

--- src/main/java/qa/automation/MyHomeWork.java
+++ src/main/java/qa/automation/MyHomeWork.java
@@ -1,10 +1,27 @@
 package qa.automation;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class MyHomeWork {
     public static void main(String[] args){
-        Scanner scanner  = new Scanner(System.in);
-    }
-}
+                System.out.println("Please, enter your first name!");
+
+            Scanner scanner  = new Scanner(System.in);
+                String firstName = scanner.next();
+                System.out.println(firstName  + " is a very nice name.");
+                System.out.println("Do you have a hobby?");
+
+                String hobby = scanner.next();
+
+                if (hobby.equals("yes")) {
+                    System.out.println("What is your hobby?");
+                    String hobbyType = scanner.next();
+                    System.out.println("Very nice.");
+            }
+                if (hobby.equals("no")) {
+                    System.out.println("How pity! You must be living a boring life?");
+            }
+        }
+        }
 
